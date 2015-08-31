81a83da8ccf0fd1fb8b1ccdbb2ac753df60f1ab9
haustraliaer <davidvonhauser@gmail.com>
2015-08-31 10:58:37 +1000

    Embedemvines

--- Frontend/RootComponent/index.js
+++ Frontend/RootComponent/index.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import style from './style.css'
 import taro from 'assets/img/taro-as-art.png'
+import vineManifest from './vineManifest.js'
 
 export default React.createClass({
   render() {
     return (
       <div className={style.root}>
-        <h1>hello bp</h1>
-        <img src={taro} />
+        <script src="https://platform.vine.co/static/scripts/embed.js"></script>
+        {this.renderVines()}
       </div>
     )
-  }
+  },
+
+  renderVines() {
+    return vineManifest.map((vine, index) => {
+      return (
+        <iframe
+          src={`https://vine.co/v/${vine.id}/embed/simple?audio=1`}
+          width="300"
+          height="300"
+          frameborder="0" />
+      )
+    })
+  },
 })
---
+++ Frontend/RootComponent/vineManifest.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    id: 'eDJxrtg32Ft'
+  },
+  {
+    id: 'eDJ2viZ5zg1'
+  },
+  {
+    id: 'eId1b75HiO0'
+  }
+]
